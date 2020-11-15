20a2bd9d4b1b96b54149252971531db58a072da6
Junjun Mao <junjun.mao@gmail.com>
2020-11-15 13:12:39 -0500

    updated

--- mcce/build.sh
+++ mcce/build.sh
@@ -2,9 +2,12 @@ make clean
 make
 
 mkdir -p $PREFIX/bin
+mkdir -p $SP_DIR/mcce
 
 cp bin/delphi $PREFIX/bin/delphi
 cp bin/mcce $PREFIX/bin/mcce
 cp bin/getpdb $PREFIX/bin/getpdb
 cp bin/*.py $PREFIX/bin/
 cp -r param $PREFIX/param
+cp name.txt $PREFIX/
+cp bin/ms_analysis.py $SP_DIR/

f967c23872c99be6346e34017e3e781155d8b070
Junjun Mao <junjun.mao@gmail.com>
2020-12-04 11:04:07 -0500

    added extra.tpl

--- mcce/build.sh
+++ mcce/build.sh
@@ -2,7 +2,7 @@ make clean
 make
 
 mkdir -p $PREFIX/bin
-mkdir -p $SP_DIR/mcce
+mkdir -p $SP_DIR
 
 cp bin/delphi $PREFIX/bin/delphi
 cp bin/mcce $PREFIX/bin/mcce
@@ -11,3 +11,4 @@ cp bin/*.py $PREFIX/bin/
 cp -r param $PREFIX/param
 cp name.txt $PREFIX/
 cp bin/ms_analysis.py $SP_DIR/
+cp extra.tpl $PREFIX/

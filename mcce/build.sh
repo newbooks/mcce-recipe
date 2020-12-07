a610b353b6f0dc88886ef8ea1e8a8b2673c7c172
Junjun Mao <junjun.mao@gmail.com>
2020-12-07 10:37:02 -0500

    both versions, meta.ymal uses different mcce branch

---
+++ mcce/build.sh
@@ -0,0 +1,15 @@
+make clean
+make
+
+mkdir -p $PREFIX/bin
+mkdir -p $SP_DIR
+
+cp bin/delphi $PREFIX/bin/delphi
+cp bin/mcce $PREFIX/bin/mcce
+cp bin/getpdb $PREFIX/bin/getpdb
+cp bin/*.py $PREFIX/bin/
+cp -r param $PREFIX/param
+cp name.txt $PREFIX/
+cp bin/ms_analysis.py $SP_DIR/
+cp extra.tpl $PREFIX/
+
